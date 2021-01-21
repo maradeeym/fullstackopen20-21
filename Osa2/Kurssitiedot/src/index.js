ab3f419ce65dba33f7e9e8f5190582ed23b9f964
Marrtin <martinkalliola@martins-mbp.lan>
2021-01-21 14:20:22 +0200

    step9

--- Osa2/Kurssitiedot/src/index.js
+++ Osa2/Kurssitiedot/src/index.js
@@ -4,30 +4,55 @@ import Course from './components/Course'
 
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 8,
-        id: 2
-      }
-    ]
-  }
-/*
-let intvalue = 0 
-let total = course.parts.reduce((acc, cur) => acc + cur.exercises, intvalue)
-console.log(total)
-*/
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
   return (
     <div>
-      <Course course={course} />
+      <h1>Web development curriculum</h1>
+      {courses.map((course) => <Course course={course}/>)}
     </div>
   )
 }
